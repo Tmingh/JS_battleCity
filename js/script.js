70f318b8b523ac1dde9bda62e54fac3da315c196
Tmingh <minghuatang@hustunique.com>
2016-03-03 20:50:55 +0800

    update wall

--- js/script.js
+++ js/script.js
@@ -1,6 +1,8 @@
 
 var canvas = document.getElementById('map'),
-    context = canvas.getContext('2d');
+    context = canvas.getContext('2d'),
+    UnitWidth = 30,
+    UnitHeight = 30;
 
 function inherit(Child, Parent) {
     var F = function(){};
@@ -61,14 +63,91 @@ imageLoader.prototype = {
         return (this.imagesLoaded + this.imagesFailedToLoad) / this.imageUrls.length * 100
     }
 };
+
+var Walls = function(props) {
+    this.images = new imageLoader();
+    this.images.imageUrls = props.urls || [];
+    this.wallPositions = props.positions || [];
+    this.initialWalls = [];
+    this.destoryedWalls = [];
+    this.wallWidth = UnitWidth / 4;
+    this.wallHeight = UnitHeight / 4;
+};
+
+Walls.prototype = {
+    init: function() {
+        this.images.loadImages();
+        this.setImage();
+        this.getInitialPositions();
+    },
+    setImage: function() {
+        for (var i = 0; i < this.wallPositions.length; i++) {
+            var wall = this.wallPositions[i];
+            var url = this.images.imageUrls[wall.id];
+            var image = this.images.getImage(url);
+
+            context.drawImage(image, wall.x, wall.y, wall.w, wall.h);
+        }
+        this.moveDestoryedWall();
+    },
+
+    moveDestoryedWall: function() {
+        for (var i = 0; i < this.destoryedWalls.length; i++) {
+            var wall = this.destoryedWalls[i];
+            var image = context.getImageData(wall.x, wall.y, this.wallWidth, this.wallHeight);
+            for (var j = 1; j <= image.data.length; j++) {
+                image.data[j*4-1] = 0;
+            }
+
+            context.putImageData(image, wall.x, wall.y);
+        }
+    },
+
+    getInitialPositions: function() {
+        for (var i = 0; i < this.wallPositions.length; i++) {
+            var wall = this.wallPositions[i];
+            if (wall.id == 0) {
+                for (var j = 0; j < 4; j++) {
+                    for (var k = 0; k < 4; k++) {
+                        this.initialWalls.push({x: wall.x+j*this.wallWidth, y: wall.y+k*this.wallHeight});
+                    }
+                }
+            } else if (wall.id == 1) {
+                this.initialWalls.push({x: wall.x, y: wall.y});
+            }
+        }
+        console.log(this.initialWalls);
+    },
+
+    checkDestoryed : function (wall) {
+        for (var i = 0; i < this.destoryedWalls.length; i++) {
+            var destoryedWall = this.destoryedWalls[i];
+            if (wall.x == destoryedWall.x && wall.y == destoryedWall.y) {
+                return 0;
+            }
+        }
+
+        return -1;
+    }
+
+};
+
+var walls = new Walls(
+    {
+        urls: ['image/walls.gif', 'image/wall.gif'],
+        positions: [{id: 0, x: 100, y: 100, w: 30, h: 30},
+            {id: 0, x: 200, y: 200, w: 30, h: 30}]
+    }
+);
+
 //坦克公用类
 var Tank = function(props) {
     this.dir = props.dir || 'up';
     this.x = props.x || 0;
     this.y = props.y || 0;
-    this.w = 30;
-    this.h = 30;
-    this.speed = 5;
+    this.w = UnitWidth;
+    this.h = UnitHeight;
+    this.speed = 3;
     this.images = new imageLoader();
     this.images.imageUrls = props.url || [];
     this.collisionState = 0;
@@ -148,17 +227,16 @@ Tank.prototype = {
 
 //子弹公用类
 var Bullet = function(props) {
-    this.dir= props.dir || 'up';
-    this.x = props.x || 0;
-    this.y = props.y || 0;
-    this.w = 8;
-    this.h = 8;
-    this.speed = 10;
-    this.bulletState = 0;
-    this.blastReason = 0;
-    this.blastState = 0;
+    var args = props || {};
+    this.dir= args.dir || 'up';
+    this.x = args.x || 0;
+    this.y = args.y || 0;
+    this.w = UnitWidth/4;
+    this.h = UnitHeight/4;
+    this.speed = 6;
+    this.shotState = false;
     this.images = new imageLoader();
-    this.images.imageUrls = props.url || [];
+    this.images.imageUrls = args.url || [];
 };
 
 Bullet.prototype = {
@@ -191,7 +269,7 @@ Bullet.prototype = {
         context.drawImage(image, this.x, this.y, this.w, this.h);
     },
 
-    fire: function(dir, x, y, w, h) {
+    shot: function(dir, x, y, w, h) {
         this.init();
         this.dir = dir;
         switch (this.dir) {
@@ -211,7 +289,55 @@ Bullet.prototype = {
                 this.x = x + w;
                 this.y = y + (h - this.h) / 2;
         }
+    },
 
+    detectBlast: function() {
+        for (var i = 0; i < walls.initialWalls.length; i++) {
+            var wall = walls.initialWalls[i];
+
+            /*if (this.dir == 'up' || this.dir == 'down') {
+                if ((this.x > wall.x - this.w && this.x < wall.x + UnitWidth + this.w) &&
+                    (this.y <= wall.y + UnitHeight && this.y >= wall.y) &&
+                    walls.checkDestoryed(wall) != 0) {
+                    this.destoryWalls(wall);
+                    //this.moveBullet();
+                    return 'blast';
+                }
+            }
+
+            if (this.dir == 'left' || this.dir == 'right') {
+                if ((this.x <= wall.x + UnitWidth && this.x >= wall.x) &&
+                    (this.y > wall.y - this.h && this.y < wall.y + UnitHeight + this.h) &&
+                    walls.checkDestoryed(wall) != 0) {
+                    this.destoryWalls(wall);
+                    //this.moveBullet();
+                    return 'blast';
+                }
+            }*/
+
+            if ( walls.checkDestoryed(wall) != 0 &&
+                this.x > wall.x-this.w &&
+                this.x < wall.x+UnitWidth/4 &&
+                this.y < wall.y+UnitHeight/4 &&
+                this.y > wall.y-this.h ) {
+
+                this.destoryWalls(wall);
+                return 'blast';
+            }
+
+        }
+
+        if (this.x >= canvas.clientWidth || this.x <= 0 ||
+            this.y >= canvas.clientHeight || this.y <= 0) {
+            return 'out';
+        } else {
+            this.move();
+            return 'in';
+        }
+    },
+
+    destoryWalls: function(wall) {
+        walls.destoryedWalls.push({x: wall.x, y: wall.y});
     }
 };
 
@@ -227,10 +353,10 @@ var EnemyBullet = function(props) {
 };
 inherit(EnemyBullet, Bullet);
 
-
 //主角类
 var Hero = function(props) {
     Tank.call(this, props);
+    this.bulletFlag = 'wait';
     this.bullet = new HeroBullet(
         {
             url: ['./image/tankmissile.gif']
@@ -243,17 +369,17 @@ Hero.prototype.actions = function() {
     this.detectStop();
     this.move();
 
-    if (this.bullet.bulletState == 1 && this.bullet.blastState == 0) {
-        this.bullet.fire(this.dir, this.x, this.y, this.w, this.h);
-        this.bullet.bulletState = 2;
-        this.bullet.blastState = 1;
+    if (this.bulletFlag == 'shot') {
+        this.bulletFlag = 'detect';
+        this.bullet.shotState = true;
+        this.bullet.shot(this.dir, this.x, this.y, this.w, this.h);
     }
-    if (this.bullet.bulletState == 2) {
-        this.bullet.move();
-        if (this.bullet.x >= canvas.offsetWidth || this.bullet.x <= 0 ||
-            this.bullet.y >= canvas.offsetHeight || this.bullet.y <= 0) {
-            this.bullet.bulletState = 0;
-            this.bullet.blastState = 0;
+
+    if (this.bulletFlag == 'detect') {
+        var bulletState = this.bullet.detectBlast();
+        if (bulletState != 'in') {
+            this.bulletFlag = 'wait';
+            this.bullet.shotState = false;
         }
     }
 
@@ -266,14 +392,17 @@ var Enemy = function(props) {
     Tank.call(this, props);
     this.dir = 'down';
     this.dirQueue = ['up', 'down', 'left', 'right'];
-    this.bullets = new Array(5);
+    this.bulletIndex = 0;
+    this.bullets = new EnemyBullet({
+        url: ['./image/enemymissile.gif']
+    });/*new Array(5);
     for (var i = 0; i < 5; i++) {
         this.bullets[i] = new EnemyBullet(
             {
                 url: ['./image/enemymissile.gif']
             }
         );
-    }
+    }*/
 };
 inherit(Enemy, Tank);
 
@@ -283,21 +412,43 @@ Enemy.prototype.actions = function() {
     if (this.collisionState == 0) {
         this.move();
     }
+
     if (this.collisionState != 0) {
         this.dirState[ this.dir ] = 0;
         var dirNum = Math.floor(Math.random()*4);
         while (this.dirQueue[dirNum] == this.dir) {
             dirNum = Math.floor(Math.random()*4);
         }
-        console.log(this.dirQueue[dirNum]);
         this.dir = this.dirQueue[dirNum];
         this.collisionState = 0;
         this.dirState[ this.dir ] = 1;
     }
 
+    this.decideFire();
+
     this.setDirection( this.dir );
 };
 
+Enemy.prototype.decideFire = function() {
+    var flag = Math.random();
+    if (flag < 0.1 && this.bullets.shotState == false) {
+        this.bullets.shot(this.dir, this.x, this.y, this.w, this.h);
+        this.bullets.shotState = true;
+
+        this.bulletIndex++;
+        if (this.bulletIndex == 5) {
+            this.bulletIndex = 0;
+        }
+    }
+    //for (var i = 0; i < this.bullets.length; i++) {
+    if (this.bullets.shotState == true) {
+        var bulletState = this.bullets.detectBlast();
+        if (bulletState != 'in') {
+            this.bullets.shotState = false;
+        }
+    }
+    //}
+};
 
 var enemy = new Enemy({
     x : 10,
@@ -312,7 +463,7 @@ var enemy = new Enemy({
 
 var hero = new Hero({
     x: 200,
-    y: 500,
+    y: 400,
     url: [
         './image/p1tankU.gif',
         './image/p1tankD.gif',
@@ -339,8 +490,8 @@ document.addEventListener('keydown', function(e) {
         hero.dirState['right'] = 1;
         hero.dir = 'right';
     }
-    if (key == 75 && hero.bullet.blastState == 0) {
-        hero.bullet.bulletState = 1;
+    if (key == 75 && hero.bulletFlag == 'wait') {
+        hero.bulletFlag = 'shot';
     }
 });
 
@@ -362,7 +513,8 @@ document.addEventListener('keyup', function(e) {
 });
 
 function play() {
-    context.clearRect(0, 0, canvas.offsetWidth, canvas.offsetHeight);
+    context.clearRect(0, 0, canvas.clientWidth, canvas.clientHeight);
+    walls.setImage();
     hero.actions();
     enemy.actions();
 }
@@ -370,5 +522,6 @@ function play() {
 window.addEventListener('load', function(e) {
     hero.init(0);
     enemy.init(1);
-    setInterval('play()', 33);
+    walls.init();
+    setInterval('play()', 20);
 });
